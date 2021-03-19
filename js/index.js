d3a1a8ec933d948a9d3ab7997bfea925cc5f9be1
Greet <vanderlindengreet@gmail.com>
2021-03-19 15:34:33 +0100

    only rotate glasses on desktop hover

--- js/index.js
+++ js/index.js
@@ -82,7 +82,6 @@ function filterKids() {
 
 function switchLightDark() {
   const sunglassesButton = document.querySelector("#darkmode");
-  const sunglasses = document.querySelector(".icon-sunglasses2");
   const containerEl = document.querySelector("#container");
   const headerEl = document.querySelector("header");
   const navTooltip = document.querySelectorAll("nav .tooltip");
@@ -91,7 +90,6 @@ function switchLightDark() {
     containerEl.classList.toggle("activeSunglasses");
     const activatedSunglasses = document.querySelector(".activeSunglasses");
     if (activatedSunglasses === null) {
-      sunglasses.style.transform = "rotate(360deg)";
       containerEl.style.backgroundColor = "#fdfdfd";
       headerEl.style.color = "#363f4c";
       navTooltip.forEach((tooltip) => {
@@ -105,7 +103,6 @@ function switchLightDark() {
         tooltip.style.border = "1px solid white";
       });
     } else {
-      sunglasses.style.transform = "rotate(360deg)";
       containerEl.style.backgroundColor = "#1C1C1C";
       headerEl.style.color = "#bcbcbc";
       navTooltip.forEach((tooltip) => {
